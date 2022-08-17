bbb6952272320e3dbe8b5f647aebcce8e66f9660
Daniel Soares <daniel.soares@me.com>
2022-08-17 08:22:08 -0300

    feat: add ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
 import {
+  Box,
   Button,
+  Center,
   Checkbox,
   FormControl,
   FormLabel,
+  Heading,
+  HStack,
+  Link,
   NumberDecrementStepper,
   NumberIncrementStepper,
   NumberInput,
   NumberInputField,
   NumberInputStepper,
+  Stack,
+  Text,
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import NextLink from "next/link";
 import { useState } from "react";
+
 import { generatePassword } from "../utils/generatePassword";
 
 const Home: NextPage = () => {
@@ -23,67 +32,100 @@ const Home: NextPage = () => {
   });
 
   return (
-    <div>
-      <h1>Password Generator</h1>
-      <h2>{password}</h2>
-      <div>
-        <FormControl>
-          <FormLabel>Length</FormLabel>
-          <NumberInput
-            value={options.length}
-            min={4}
-            max={64}
-            onChange={(value) => setOptions({ ...options, length: +value })}
-          >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-        </FormControl>
+    <Center bg="gray.100" h="100vh" w="full" flexDirection="column">
+      <Box p={8} bg="gray.50" rounded={4} shadow="md">
+        <Heading textAlign="center">Password Generator</Heading>
 
-        <FormControl>
-          <Checkbox
-            isChecked={options.upperCase}
-            onChange={(e) =>
-              setOptions({ ...options, upperCase: e.target.checked })
-            }
-          >
-            With upper case
-          </Checkbox>
-        </FormControl>
+        <Center bg="gray.600" rounded="md" px={2} h="10" my={6}>
+          <Text color="gray.50" fontWeight="bold" fontSize="xl">
+            {password}
+          </Text>
+        </Center>
+        <HStack gap={4}>
+          <Stack gap={3}>
+            <FormControl>
+              <Checkbox
+                isChecked={options.upperCase}
+                onChange={(e) =>
+                  setOptions({ ...options, upperCase: e.target.checked })
+                }
+              >
+                With upper case
+              </Checkbox>
+            </FormControl>
 
-        <FormControl>
-          <Checkbox
-            isChecked={options.numbers}
-            onChange={(e) =>
-              setOptions({ ...options, numbers: e.target.checked })
-            }
-          >
-            With numbers
-          </Checkbox>
-        </FormControl>
+            <FormControl>
+              <Checkbox
+                isChecked={options.numbers}
+                onChange={(e) =>
+                  setOptions({ ...options, numbers: e.target.checked })
+                }
+              >
+                With numbers
+              </Checkbox>
+            </FormControl>
 
-        <FormControl>
-          <Checkbox
-            isChecked={options.specialCharacters}
-            onChange={(e) =>
-              setOptions({ ...options, specialCharacters: e.target.checked })
-            }
-          >
-            With special characters
-          </Checkbox>
-        </FormControl>
+            <FormControl>
+              <Checkbox
+                isChecked={options.specialCharacters}
+                onChange={(e) =>
+                  setOptions({
+                    ...options,
+                    specialCharacters: e.target.checked,
+                  })
+                }
+              >
+                With special characters
+              </Checkbox>
+            </FormControl>
+          </Stack>
 
-        <Button
-          colorScheme="blue"
-          onClick={() => setPassword(generatePassword(options))}
-        >
-          Generate password
-        </Button>
-      </div>
-    </div>
+          <Stack gap={2}>
+            <FormControl>
+              <FormLabel>Length</FormLabel>
+              <NumberInput
+                value={options.length}
+                min={4}
+                max={32}
+                onChange={(value) => setOptions({ ...options, length: +value })}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+
+            <Button
+              colorScheme="blue"
+              onClick={() => setPassword(generatePassword(options))}
+            >
+              Generate password
+            </Button>
+          </Stack>
+        </HStack>
+      </Box>
+
+      <Stack
+        as="footer"
+        textAlign="center"
+        fontSize="sm"
+        gap={0}
+        lineHeight={1}
+        mt={4}
+      >
+        <Text>
+          By <strong>Daniel Soares</strong>
+        </Text>
+        <Text>
+          Source code on{" "}
+          <NextLink href="https://github.com" passHref>
+            <Link>Github</Link>
+          </NextLink>
+        </Text>
+      </Stack>
+    </Center>
   );
 };
 

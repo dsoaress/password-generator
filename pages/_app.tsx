bd7d6189a0f444ebfb4df22040780b8cdff61ca0
Daniel Soares <daniel.soares@me.com>
2022-08-17 08:25:09 -0300

    chore: add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Password Generator</title>
+      </Head>
+
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
